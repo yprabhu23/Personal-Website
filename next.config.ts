fa4c28bb7b5a80a3e9ab86d6d57bdeffff169cd1
Yash Prabhu <yprabhu@mit.edu>
2025-07-14 19:47:44 -0400

    Removed base path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Required for static HTML export
-  basePath: '',
   images: {
     unoptimized: true,
   },
