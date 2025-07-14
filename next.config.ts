3a5d966514fb7e5bb5317f5ccae2a435f659397d
Yash Prabhu <yprabhu@mit.edu>
2025-07-14 19:49:42 -0400

    New path added

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Required for static HTML export
+  basePath: '/yprabhu23.github.io',
   images: {
     unoptimized: true,
   },
