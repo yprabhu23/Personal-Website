d4ab6c1baf6192efd27035d9e507364754792d3e
Yash Prabhu <yprabhu@mit.edu>
2025-07-14 20:34:57 -0400

    Removed base path for online domain

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Required for static HTML export
-  basePath: '/Personal-Website',
-  assetPrefix: '/Personal-Website',
+  // basePath: '/Personal-Website',
+  // assetPrefix: '/Personal-Website',
   images: {
     unoptimized: true,
   },
