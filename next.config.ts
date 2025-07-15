e7f76f38d3b5ba2607e93f3dd9b8edbcc206b683
Yash Prabhu <yprabhu@mit.edu>
2025-07-14 20:06:06 -0400

    Re built

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Required for static HTML export
   basePath: '/Personal-Website',
+  assetPrefix: '/Personal-Website',
   images: {
     unoptimized: true,
   },
